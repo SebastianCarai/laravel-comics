c253814f767fb99b07ba5accf187060711be52b2
SebastianCarai <sebastiancarai7@gmail.com>
2022-02-09 17:38:41 +0100

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,33 @@
 <footer>
     <div class="footer_top d_flex">
-        <div>
-            ciao
+        <div class="my_container">
+            {{-- Footer list --}}
+            <div class="footer_list">
+                <h3 class="footer_title">DC COMICS</h3>
+                <ul>
+                    <li class="footer_link">
+                        <a href="#">Characters</a>
+                    </li>
+                    <li class="footer_link">
+                        <a href="#">Characters</a>
+                    </li>
+                    <li class="footer_link">
+                        <a href="#">Characters</a>
+                    </li>
+                    <li class="footer_link">
+                        <a href="#">Characters</a>
+                    </li>
+                    <li class="footer_link">
+                        <a href="#">Characters</a>
+                    </li>
+                    <li class="footer_link">
+                        <a href="#">Characters</a>
+                    </li>
+                    <li class="footer_link">
+                        <a href="#">Characters</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
     <div class="footer_bottom">
