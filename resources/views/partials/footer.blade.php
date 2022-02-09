7ab2acf6167e382248fa13f2a747972a26ac3c12
SebastianCarai <sebastiancarai7@gmail.com>
2022-02-09 17:44:32 +0100

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,32 +1,117 @@
 <footer>
-    <div class="footer_top d_flex">
-        <div class="my_container">
-            {{-- Footer list --}}
-            <div class="footer_list">
-                <h3 class="footer_title">DC COMICS</h3>
-                <ul>
-                    <li class="footer_link">
-                        <a href="#">Characters</a>
-                    </li>
-                    <li class="footer_link">
-                        <a href="#">Characters</a>
-                    </li>
-                    <li class="footer_link">
-                        <a href="#">Characters</a>
-                    </li>
-                    <li class="footer_link">
-                        <a href="#">Characters</a>
-                    </li>
-                    <li class="footer_link">
-                        <a href="#">Characters</a>
-                    </li>
-                    <li class="footer_link">
-                        <a href="#">Characters</a>
-                    </li>
-                    <li class="footer_link">
-                        <a href="#">Characters</a>
-                    </li>
-                </ul>
+    <div class="footer_top">
+        <div class="my_container d_flex">
+            <div>
+                {{-- Footer list --}}
+                <div class="footer_list">
+                    <h3 class="footer_title">DC COMICS</h3>
+                    <ul>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                    </ul>
+                </div>
+                {{-- End footer list --}}
+
+                {{-- Footer list --}}
+                <div class="footer_list">
+                    <h3 class="footer_title">DC COMICS</h3>
+                    <ul>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                    </ul>
+                </div>
+            {{-- End footer list --}}
+            </div>
+
+            <div>
+                {{-- Footer list --}}
+                <div class="footer_list">
+                    <h3 class="footer_title">DC COMICS</h3>
+                    <ul>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                    </ul>
+                </div>
+                {{-- End footer list --}}
+            </div>
+
+            <div>
+                {{-- Footer list --}}
+                <div class="footer_list">
+                    <h3 class="footer_title">DC COMICS</h3>
+                    <ul>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                        <li class="footer_link">
+                            <a href="#">Characters</a>
+                        </li>
+                    </ul>
+                </div>
+                {{-- End footer list --}}
             </div>
         </div>
     </div>
